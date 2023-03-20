5b57e12a32a77d4677e61638402ddffd2634a452
otabekaripdjanov <597066@park.edu>
2023-03-19 23:47:20 -0400

    clean up

--- src/ContestDriver.java
+++ src/ContestDriver.java
@@ -1,26 +1,32 @@
-import java.lang.constant.Constable;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class ContestDriver {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         ArrayList<Contestant> contestants = new ArrayList<Contestant>();
 
-        int numberOfContestants = 3;
+        int numberOfContestants = 45000;
         for (int i = 0; i < numberOfContestants; i++) {
             Contestant newContestant = new Contestant("Contestant#" + i , i);
             contestants.add(newContestant);
         }
 
-        ArrayList<Contestant> game = playAGame(contestants);
+        playAGame(contestants);
         System.out.println("Finished");
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Elapsed time: " + elapsedTime + " ms");
         // Print current state of contestants
-        for (String record: game.get(0).getRecords()) {
-            System.out.println(record);
+        for (var contestant : contestants) {
+            System.out.println(contestant.getRecord());
         }
     }
 
-    public static ArrayList<Contestant> playAGame(ArrayList<Contestant> contestants) {
+    public static void playAGame(ArrayList<Contestant> contestants) {
         System.out.println("Game started!");
         int currentContestantNumber = 0;
 
@@ -33,9 +39,9 @@ public class ContestDriver {
                 if (currentContestantNumber != 0) {
                     contestants.remove(currentContestant);
                     contestants.add(0, currentContestant);
-                    currentContestant.addRecord(getCurrentStand(contestants) + ", Correct Answer and moving " + currentContestant.getName() + " to the front!");
+                    currentContestant.addRecord("Correct Answer and moving " + currentContestant.getName() + " to the front!");
                 } else {
-                    currentContestant.addRecord(getCurrentStand(contestants) + ", Correct Answer but " + currentContestant.getName() + " already in front!");
+                    currentContestant.addRecord("Correct Answer but " + currentContestant.getName() + " already in front!");
                 }
                 // moving to the next player
                 currentContestantNumber += 1;
@@ -44,30 +50,17 @@ public class ContestDriver {
                 if (currentContestantNumber != contestants.size()-1) {
                     contestants.remove(currentContestant);
                     contestants.add(currentContestant);
-                    currentContestant.addRecord(getCurrentStand(contestants) + ", Wrong answer! Moving " + currentContestant.getName() + " to the end!");
+                    currentContestant.addRecord("Wrong answer! Moving " + currentContestant.getName() + " to the end!");
                 } else {
-                    currentContestant.addRecord(getCurrentStand(contestants) + ", Wrong Answer but " + currentContestant.getName() + " already last!");
+                    currentContestant.addRecord("Wrong Answer but " + currentContestant.getName() + " already last!");
                 }
             }
         }
-        return contestants;
     }
 
     public static Boolean flipACoin() {
         Random rand = new Random();
         Boolean answer = rand.nextBoolean();
         return answer;
-    }
-
-    private static Constable getCurrentStand(ArrayList<Contestant> contestants) {
-        StringBuilder stand = new StringBuilder("[");
-        for (int i = 0; i < contestants.size(); i++) {
-            if (i != contestants.size() - 1) {
-                stand.append(contestants.get(i).getName() + ", ");
-            } else {
-                stand.append(contestants.get(i).getName() + "]");
-            }
-        }
-        return stand.toString();
     }
 }
--- src/Contestant.java
+++ src/Contestant.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /* Contestant.java:
  * This class extends Person while adding attributes and methods
  * related to recording performance in the specified contest.
@@ -8,30 +6,16 @@ import java.util.ArrayList;
  * Last updated:
  */
 public class Contestant extends Person {
-    public static  ArrayList<String> records = new ArrayList<String>();
+    private String record;
     public Contestant(String name, int age) {
         super(name, age);
     }
-    // A Contestant IS-A Person
-    // that also needs to keep track of data related to the game
-    // Start simple to get your program running, then add attributes and methods as you need them
-
-    // Step 1 Add at least one constructor so the driver will compile and run
 
     public void addRecord(String message) {
-        this.records.add(message);
+        this.record = message;
     }
 
-//    private ArrayList<Contestant>  unpackCurrentStand(ArrayList<Contestant> contestants) {
-//        ArrayList<Contestant> currentStand = new ArrayList<Contestant>();
-//        for (Contestant contestant: contestants) {
-//            currentStand.add(contestant);
-//        }
-//        return currentStand;
-//    }
-    // Step 5 Add attributes and methods to assist with recording game statistics
-
-    public ArrayList<String> getRecords() {
-       return this.records;
+    public String getRecord() {
+       return this.record;
     }
 }
